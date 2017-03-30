e52ac4f0355a9819080667766afb76797bb55839
Jeffrey Li <jeffrey.li@cantab.net>
2017-03-30 17:17:02 +0800

    done phone class

--- lib/phone.rb
+++ lib/phone.rb
@@ -10,4 +10,26 @@ class Phone
 
   attr_reader(:area_code, :number, :type, :id)
 
+  define_singleton_method(:all) do
+    @@phones
+  end
+
+  define_method(:save) do
+    @@phones.push(self)
+  end
+
+  define_singleton_method(:clear) do
+    @@phones = []
+  end
+
+  define_singleton_method(:find) do |id|
+    found_phone = nil
+    @@phones.each() do |phone|
+      if phone.id().eql?(id.to_i())
+        found_phone = phone
+      end
+    end
+    found_phone
+  end
+
 end #end of Phone class
--- spec/phone_spec.rb
+++ spec/phone_spec.rb
@@ -4,6 +4,7 @@ require('phone')
 describe(Phone) do
 
   before() do
+    Phone.clear()
     @test_phone = Phone.new({:area_code => 852, :number => 23456789, :type => "home"})
   end
 
@@ -13,6 +14,34 @@ describe(Phone) do
       expect(@test_phone.number()).to(eq(23456789))
       expect(@test_phone.type()).to(eq("home"))
       expect(@test_phone.id()).to(eq(1))
+    end
+  end
+
+  describe(".all") do
+    it("Phone array is empty at first") do
+      expect(Phone.all()).to(eq([]))
+    end
+  end
+
+  describe("#save") do
+    it("adds a CD to the array of CDs") do
+      @test_phone.save()
+      expect(Phone.all()).to(eq([@test_phone]))
+    end
+  end
+
+  describe(".clear") do
+    it("empties out all of the saved phone numbers") do
+      @test_phone.save()
+      Phone.clear()
+      expect(Phone.all()).to(eq([]))
+    end
+  end
+
+  describe(".find") do
+    it("returns a phone number by the id number") do
+      @test_phone.save()
+      expect(Phone.find(@test_phone.id())).to(eq(@test_phone))
     end
   end
 
